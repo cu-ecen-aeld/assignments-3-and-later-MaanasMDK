0362e5adbb2b0fd9fc7a24ac9ad13925ee48d0ba
MaanasMDK <mama9908@colorado.edu>
2023-03-18 12:53:05 -0600

    A8 : Edited driver code

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -7,6 +7,7 @@
 
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
+#include "aesd-circular-buffer.h"
 
 #define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
@@ -28,7 +29,11 @@ struct aesd_dev
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
-    struct cdev cdev;     /* Char device structure      */
+     struct aesd_circular_buffer buffer;
+     struct mutex lock;
+     char *buff_ptr;
+     int buff_size;
+     struct cdev cdev;     /* Char device structure      */
 };
 
 
